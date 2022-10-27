598d630cd456f7057d82419e99ec0951d7c75001
lordmavles <zachmavles@gmail.com>
2022-10-27 23:35:17 +0530

    sub event model and repository

---
+++ src/main/java/com/example/fms/festmanagement/doa/SubEventRepository.java
@@ -0,0 +1,38 @@
+package com.example.fms.festmanagement.doa;
+
+import com.example.fms.festmanagement.models.SubEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SubEventRepository {
+
+    @Autowired
+    private JdbcTemplate t;
+
+    public void initSubevent() {
+
+        String x = "CREATE TABLE IF NOT EXISTS SubEvent (subeventId INT, startDate INT, endDate INT, venue String, evenId INT))";
+
+        t.update(x);
+
+    }
+
+    public void insertSubevent(SubEvent subevent) {
+
+        String x = "INSERT INTO SubEvent VALUES (?, ?, ?, ?, ?)";
+
+        t.update(x, subevent.getSubeventId(), subevent.getStartDate(), subevent.getEndDate(), subevent.getVenue(), subevent.getEventId());
+
+    }
+
+    public void deleteSubevent(int subeventId) {
+
+        String x = "DELETE FROM SubEvent WHERE subeventId = ?";
+
+        t.update(x, subeventId);
+
+    }
+
+}
---
+++ src/main/java/com/example/fms/festmanagement/models/SubEvent.java
@@ -0,0 +1,57 @@
+package com.example.fms.festmanagement.models;
+
+import java.util.Date;
+
+public class SubEvent {
+
+    private int subeventId;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private String venue;
+
+    private int eventId;
+
+    public int getSubeventId() {
+        return subeventId;
+    }
+
+    public void setSubeventId(int subeventId) {
+        this.subeventId = subeventId;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+}
