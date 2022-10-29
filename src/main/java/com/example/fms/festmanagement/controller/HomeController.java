3eafd68b62f6c38e6a2e29b871c4e693f8fd85d6
lordmavles <zachmavles@gmail.com>
2022-10-29 15:21:35 +0530

    fdghj

--- src/main/java/com/example/fms/festmanagement/controller/HomeController.java
+++ src/main/java/com/example/fms/festmanagement/controller/HomeController.java
@@ -3,9 +3,13 @@ package com.example.fms.festmanagement.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @Transactional
+@RestController
 public class HomeController {
 
     @GetMapping("/")
