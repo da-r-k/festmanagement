9cff2a5cc6aca33dacc7856637cd5713aebb2bc8
Dark <soumyadwip@outlook.in>
2022-10-29 15:23:50 +0530

    RestController to helloworld

--- src/main/java/com/example/fms/festmanagement/controller/HomeController.java
+++ src/main/java/com/example/fms/festmanagement/controller/HomeController.java
@@ -3,9 +3,11 @@ package com.example.fms.festmanagement.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @Transactional
+@RestController
 public class HomeController {
 
     @GetMapping("/")
