13d9076e1e8723746edfd709f47ec685aa4aabeb
Dark <soumyadwip@outlook.in>
2022-10-29 15:57:25 +0530

    Added home.html

--- src/main/java/com/example/fms/festmanagement/controller/HomeController.java
+++ src/main/java/com/example/fms/festmanagement/controller/HomeController.java
@@ -2,21 +2,21 @@ package com.example.fms.festmanagement.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @Transactional
-@RestController
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-
-        return "Hello World";
-
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String home(Model model) {
+        return "home";
     }
 
 }
