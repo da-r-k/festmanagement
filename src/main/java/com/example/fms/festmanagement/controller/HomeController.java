7175ec53c0625c5c361ed702fcd75926539d42cc
Dark <soumyadwip@outlook.in>
2022-10-29 16:07:07 +0530

    Thymeleaf rendering correctly

--- src/main/java/com/example/fms/festmanagement/controller/HomeController.java
+++ src/main/java/com/example/fms/festmanagement/controller/HomeController.java
@@ -4,17 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @Transactional
 public class HomeController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
+    @GetMapping("/")
     public String home(Model model) {
         return "home";
     }
